6fb004ab8dd2835989b308fbcf33a4bf9d7d7ecd
Goohong Chung <9rrong@uoslife.team>
2024-10-09 16:15:43 +0900

    test: ci/cd test

--- src/main/kotlin/sogoing/backend_server/app/user/api/UserController.kt
+++ src/main/kotlin/sogoing/backend_server/app/user/api/UserController.kt
@@ -1,13 +1,14 @@
 package sogoing.backend_server.app.user.api
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UserController {
 
-  @GetMapping("/")
-  fun testCall(): String {
-    return "success - 2"
+  @GetMapping("/{test}")
+  fun testCall(@PathVariable test: String): String {
+    return "success - $test"
   }
 }
