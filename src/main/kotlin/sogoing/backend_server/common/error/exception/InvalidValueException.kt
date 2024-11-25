d42987d67b33d193c18fabcbb9f5ae422a5db3fe
Goohong Chung <9rrong@uoslife.team>
2024-11-25 15:43:27 +0900

    feat: add exception handling logic

---
+++ src/main/kotlin/sogoing/backend_server/common/error/exception/InvalidValueException.kt
@@ -0,0 +1,16 @@
+package sogoing.backend_server.common.error.exception
+
+import sogoing.backend_server.common.error.ErrorCode
+
+open class InvalidValueException : RuntimeException {
+
+    val errorCode: ErrorCode
+
+    constructor(message: String, errorCode: ErrorCode) : super(message) {
+        this.errorCode = errorCode
+    }
+
+    constructor(errorCode: ErrorCode) : super(errorCode.message) {
+        this.errorCode = errorCode
+    }
+}
---
+++ src/main/kotlin/sogoing/backend_server/common/error/exception/user/UserEmailAlreadyExistingException.kt
@@ -0,0 +1,7 @@
+package sogoing.backend_server.common.error.exception.user
+
+import sogoing.backend_server.common.error.ErrorCode
+import sogoing.backend_server.common.error.exception.AccessDeniedException
+
+class UserEmailAlreadyExistingException : AccessDeniedException(ErrorCode.USER_EMAIL_ALREADY_EXISTING) {
+}
