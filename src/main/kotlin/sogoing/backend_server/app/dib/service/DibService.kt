7eb5520611ac674dec36706905fc703879f53746
woojin <zaza0804@uos.ac.kr>
2024-12-03 18:18:53 +0900

    feat: 엔티티 에러 추가, 찜 조회 여부 API 추가

--- src/main/kotlin/sogoing/backend_server/app/dib/service/DibService.kt
+++ src/main/kotlin/sogoing/backend_server/app/dib/service/DibService.kt
@@ -1,35 +1,50 @@
 package sogoing.backend_server.app.dib.service
 
-import java.lang.IllegalStateException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import sogoing.backend_server.app.department.repository.DepartmentRepository
 import sogoing.backend_server.app.dib.dao.DibDao
+import sogoing.backend_server.app.dib.dto.DibChangeResponse
+import sogoing.backend_server.app.dib.dto.DibStatusResponse
 import sogoing.backend_server.app.dib.entity.Dib
 import sogoing.backend_server.app.dib.repository.DibRepository
 import sogoing.backend_server.app.user.repository.UserRepository
+import sogoing.backend_server.common.error.exception.department.DepartmentNotFoundException
+import sogoing.backend_server.common.error.exception.user.UserNotFoundException
 
 @Service
+@Transactional(readOnly = true)
 class DibService(
     private val dibRepository: DibRepository,
     private val dibDao: DibDao,
     private val userRepository: UserRepository,
     private val departmentRepository: DepartmentRepository
 ) {
-    fun changeDibs(departmentId: Long, userId: Long): Long? {
-        val user = userRepository.findByIdOrNull(userId) ?: throw IllegalStateException()
+    @Transactional
+    fun changeDibs(departmentId: Long, userId: Long): DibChangeResponse {
+        val user = userRepository.findByIdOrNull(userId) ?: throw UserNotFoundException()
         val userDibs = dibDao.getDibFromUserIdAndDepartmentId(userId, departmentId)
 
         if (userDibs != null) {
             dibRepository.delete(userDibs)
-            return userDibs.id
+            return DibChangeResponse(false)
         }
 
         val department =
-            departmentRepository.findByIdOrNull(departmentId)
-                ?: throw IllegalStateException("부서 없음")
+            departmentRepository.findByIdOrNull(departmentId) ?: throw DepartmentNotFoundException()
         val newUserDibs = Dib.create(user, department)
         dibRepository.save(newUserDibs)
-        return newUserDibs.id
+        return DibChangeResponse(true)
+    }
+
+    fun getDibs(departmentId: Long, userId: Long): DibStatusResponse {
+        departmentRepository.findByIdOrNull(departmentId) ?: throw DepartmentNotFoundException()
+        val userDibs = dibDao.getDibFromUserIdAndDepartmentId(userId, departmentId)
+
+        userDibs?.let {
+            return DibStatusResponse(true)
+        }
+        return DibStatusResponse(false)
     }
 }
---
+++ src/main/kotlin/sogoing/backend_server/common/error/exception/EntityNotFoundException.kt
@@ -0,0 +1,15 @@
+package sogoing.backend_server.common.error.exception
+
+import sogoing.backend_server.common.error.ErrorCode
+
+open class EntityNotFoundException : RuntimeException {
+    val errorCode: ErrorCode
+
+    constructor(message: String, errorCode: ErrorCode) : super(message) {
+        this.errorCode = errorCode
+    }
+
+    constructor(errorCode: ErrorCode) : super(errorCode.message) {
+        this.errorCode = errorCode
+    }
+}
