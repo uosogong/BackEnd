d7829a73da48c08d07d28ce0fe77a7d0e19422ae
goohong <goohong.chung@gmail.com>
2024-12-06 15:35:57 +0900

    fix: add phone field in sign up request

--- src/main/kotlin/sogoing/backend_server/app/auth/dto/SignUpRequest.kt
+++ src/main/kotlin/sogoing/backend_server/app/auth/dto/SignUpRequest.kt
@@ -7,4 +7,5 @@ data class SignUpRequest(
     @Schema(description = "회원 이메일", example = "foo@bar.com") val email: String,
     @Schema(description = "회원 비밀번호", example = "1234") val password: String,
     @Schema(description = "회원 학번", example = "2020920055") val studentId: String,
+    @Schema(description = "회원 전화번호", example = "01012345678") val phone: String,
 )
