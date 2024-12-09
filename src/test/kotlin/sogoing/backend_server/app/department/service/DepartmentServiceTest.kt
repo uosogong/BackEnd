ec2f59cbe63354257ccfd5c8b9a1e3725b6debad
woojin <zaza0804@uos.ac.kr>
2024-12-10 01:22:54 +0900

    style: apply convention

--- src/test/kotlin/sogoing/backend_server/app/department/service/DepartmentServiceTest.kt
+++ src/test/kotlin/sogoing/backend_server/app/department/service/DepartmentServiceTest.kt
@@ -1,20 +1,16 @@
 package sogoing.backend_server.app.department.service
 
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
-
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.mockito.Mockito.*
-import sogoing.backend_server.app.department.repository.DepartmentRepository
-
 import io.mockk.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.*
 import org.springframework.boot.test.context.SpringBootTest
 import sogoing.backend_server.app.department.entity.Department
+import sogoing.backend_server.app.department.repository.DepartmentRepository
 import sogoing.backend_server.app.dib.dto.DibStatusResponse
 import sogoing.backend_server.app.dib.service.DibService
 import sogoing.backend_server.app.feedback.dto.FeedbackResponseDto
-import sogoing.backend_server.app.feedback.entity.Feedback
 import sogoing.backend_server.app.feedback.service.FeedbackService
 import sogoing.backend_server.app.user.entity.User
 
@@ -23,14 +19,15 @@ class DepartmentServiceTest {
     private val departmentRepository: DepartmentRepository = mockk()
     private val dibService: DibService = mockk()
     private val feedbackService: FeedbackService = mockk()
-    private val departmentService = DepartmentService(
-        encoder = mockk(),
-        departmentRepository = departmentRepository,
-        userRepository = mockk(),
-        departmentDao = mockk(),
-        feedbackService = feedbackService,
-        dibService = dibService
-    )
+    private val departmentService =
+        DepartmentService(
+            encoder = mockk(),
+            departmentRepository = departmentRepository,
+            userRepository = mockk(),
+            departmentDao = mockk(),
+            feedbackService = feedbackService,
+            dibService = dibService
+        )
 
     @Test
     fun `should return empty department list when repository is empty`() {
@@ -49,13 +46,30 @@ class DepartmentServiceTest {
         val mockuser1 = mockk<User>()
         val mockuser2 = mockk<User>()
         // Arrange
-        val departments = listOf(
-            Department(id = 1L, name = "Dept A", introduction = "aa", internRecruitment = false, scholarshipRecruitment = false, user = mockuser1),
-            Department(id = 2L, name = "Dept B", introduction = "aa", internRecruitment = false, scholarshipRecruitment = false, user = mockuser2)
-        )
+        val departments =
+            listOf(
+                Department(
+                    id = 1L,
+                    name = "Dept A",
+                    introduction = "aa",
+                    internRecruitment = false,
+                    scholarshipRecruitment = false,
+                    user = mockuser1
+                ),
+                Department(
+                    id = 2L,
+                    name = "Dept B",
+                    introduction = "aa",
+                    internRecruitment = false,
+                    scholarshipRecruitment = false,
+                    user = mockuser2
+                )
+            )
         every { departmentRepository.findAllByOrderByUpdatedDateDesc() } returns departments
-        every { feedbackService.findDepartmentFeedbacks(1L) } returns FeedbackResponseDto.FeedbackListDto()
-        every { feedbackService.findDepartmentFeedbacks(2L) } returns FeedbackResponseDto.FeedbackListDto()
+        every { feedbackService.findDepartmentFeedbacks(1L) } returns
+            FeedbackResponseDto.FeedbackListDto()
+        every { feedbackService.findDepartmentFeedbacks(2L) } returns
+            FeedbackResponseDto.FeedbackListDto()
         every { dibService.getDib(1L, 100L) } returns DibStatusResponse(false)
         every { dibService.getDib(2L, 100L) } returns DibStatusResponse(true)
 
@@ -64,7 +78,7 @@ class DepartmentServiceTest {
 
         // Assert
         assertEquals(2, result.departments.size)
-//        assertEquals(3.5F, result.departments[0])
-//        assertEquals(true, result.departments[0].isUserDibs)
+        //        assertEquals(3.5F, result.departments[0])
+        //        assertEquals(true, result.departments[0].isUserDibs)
     }
 }
